d9cde339b59636d96220229cadc0c777cb705673
SergiCB99 <sergicampanalesbalana@gmail.com>
2021-07-17 15:56:43 +0200

    Create Conductor.java

---
+++ M3-SergiC/Conductor.java
@@ -0,0 +1,8 @@
+
+public class Conductor extends Persona {
+
+	public Conductor (String nombre,String apellido,int fecha_nacimiento,Licencia licencia) {
+		super(nombre,apellido,fecha_nacimiento,licencia);
+	}
+	
+}
