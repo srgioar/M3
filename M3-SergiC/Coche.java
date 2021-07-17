f3e8630db47e7f6cce289d080db12dafffa54f13
SergiCB99 <sergicampanalesbalana@gmail.com>
2021-07-17 15:56:35 +0200

    Create Coche.java

---
+++ M3-SergiC/Coche.java
@@ -0,0 +1,41 @@
+
+public class Coche extends Vehiculo {
+
+	protected Rueda rueda_modelo;
+	
+	public Coche(Titular titular,String matricula, String marca, String color) {
+
+        super(titular,matricula, marca, color);
+
+        this.rueda_modelo = Rueda.generarRueda();
+        
+        this.ejes = 2;
+        this.ruedas_delanteras = new Rueda[2];
+        this.ruedas_traseras = new Rueda[2];
+        this.ruedas_traseras[0] = this.rueda_modelo;
+        this.ruedas_traseras[1] = this.rueda_modelo;
+        this.ruedas_delanteras[0] = this.rueda_modelo;
+        this.ruedas_delanteras[1] = this.rueda_modelo;
+    }
+	
+	public String toString() {
+		
+		String nombreTitular = "";
+		
+		if (titular == null) {
+			nombreTitular = "Sin titular \n";
+		}
+		
+		else {
+			nombreTitular = titular.nombre + "\n";
+		}
+		
+		return "Coche Info:\n" + "TITULAR: " + nombreTitular +
+				"MATRÍCULA: " + matricula + "\n MARCA: " + marca + "\n COLOR: " + color +
+				"\n CANTIDAD RUEDAS DELANTERAS: 2" +
+				"\n CANTIDAD RUEDAS TRASERAS: 2" +
+				"\n RUEDAS INSTALADAS: " + this.ruedas_delanteras[0].toString() +
+				"\n CANTIDAD DE EJES: 2 (Coche)";
+	}
+	
+}
