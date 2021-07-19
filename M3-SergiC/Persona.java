d7a70fbc8c0131d899f71ddafef4a0c0f334b659
SergiCB99 <sergicampanalesbalana@gmail.com>
2021-07-19 08:26:39 +0200

    Update Persona.java

--- M3-SergiC/Persona.java
+++ M3-SergiC/Persona.java
@@ -12,4 +12,37 @@ public abstract class Persona {
 		this.fecha_nacimiento=fecha_nacimiento;
 		this.licencia=licencia;
 	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public int getFecha_nacimiento() {
+		return fecha_nacimiento;
+	}
+
+	public void setFecha_nacimiento(int fecha_nacimiento) {
+		this.fecha_nacimiento = fecha_nacimiento;
+	}
+
+	public Licencia getLicencia() {
+		return licencia;
+	}
+
+	public void setLicencia(Licencia licencia) {
+		this.licencia = licencia;
+	}
+	
 }
