8df15b503b9cfe4a07bc3b3cf9567f439eec2d61
SergiCB99 <sergicampanalesbalana@gmail.com>
2021-07-19 08:25:58 +0200

    Update Licencia.java

--- M3-SergiC/Licencia.java
+++ M3-SergiC/Licencia.java
@@ -18,6 +18,7 @@ public class Licencia {
 	}
 	
 	public static Licencia crearLicencia() {
+		JOptionPane.showMessageDialog(null, "Crear Licencia");
 		String ID_usuario = JOptionPane.showInputDialog("ID");
 		int ID = Integer.parseInt(ID_usuario);
 		String tipo = JOptionPane.showInputDialog("Tipo");
@@ -30,5 +31,39 @@ public class Licencia {
 		return licencia;
 		
 	}
+
+	public int getID() {
+		return ID;
+	}
+
+	public void setID(int iD) {
+		ID = iD;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getNombre_completo() {
+		return nombre_completo;
+	}
+
+	public void setNombre_completo(String nombre_completo) {
+		this.nombre_completo = nombre_completo;
+	}
+
+	public int getFecha_caducidad() {
+		return fecha_caducidad;
+	}
+
+	public void setFecha_caducidad(int fecha_caducidad) {
+		this.fecha_caducidad = fecha_caducidad;
+	}
+	
+	
 	
 }
